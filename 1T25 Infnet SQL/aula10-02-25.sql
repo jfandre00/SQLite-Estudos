f7165809e60eccbda05a6279c4c1a64fdf2cda16
jfandre00 <jfandre@gmail.com>
2025-02-11 10:09:07 -0500

    aula 10-02-25

---
+++ 1T25 Infnet SQL/aula10-02-25.sql
@@ -0,0 +1,72 @@
+-- alunos - id (pk), cpf (unico), nome, email (unico), nascimento que deve ser uma data válida (não pode ser futura)
+-- cursos - id (pk), nome (unico), carga_horaria (maior que zero), valor_curso (maior igual a zero)
+-- relacionamento: um aluno pode se matricular em varios cursos
+--                 um curso pode ter varios cursos matriculados (aluno e curso: muitos para muitos)
+--                 data matricula é obrigatória e deve ser maior ou igual a zero
+-- p/ muitos p muitos: criar uma tabela auxiliar TMatricula
+
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE TAluno(
+    id INTEGER primary key autoincrement,
+    cpf TEXT unique not null,
+    nome TEXT not null,
+    email TEXT unique not null,
+    dataNascimento DATE not null --check(dataNascimento <= Date('now'))
+    );
+    
+CREATE TABLE TCurso(
+    id INTEGER primary key autoincrement,
+    nome TEXT unique not null,
+    cargaHoraria INTEGER not null check(cargaHoraria > 0),
+    valor REAL not null check(valor >= 0)
+    );
+    
+CREATE TABLE TMatricula(
+    id INTEGER primary key autoincrement,
+    idAluno INTEGER,
+    idCurso INTEGER,
+    dataMatricula TEXT,
+    foreign key (idAluno) references TALUNO(id)
+    foreign key (idCurso) references TCurso);
+    
+INSERT into TAluno (cpf, nome, email, dataNascimento)
+VALUES('123.456.789-00', 'João Silva', 'joao.silva@exemplo.com', '1990-01-01');
+INSERT into TAluno (cpf, nome, email, dataNascimento)
+VALUES('321.434.789-00', 'Maria Silva', 'maria.silva@exemplo.com', '1990-02-01');
+
+--select * from TAluno;
+
+INSERT into TCurso(nome, cargaHoraria, valor)
+VALUES('Introducao SQL', 40, 299.90);
+
+INSERT into TCurso(nome, cargaHoraria, valor)
+VALUES('Desenvolvimento WEB', 60, 499.90);
+
+--select * from TCurso;
+
+INSERT into TMatricula (idAluno, idCurso, dataMatricula)
+VALUES(1, 1, '2023-10-01');
+
+
+INSERT into TMatricula (idAluno, idCurso, dataMatricula)
+VALUES(2, 2, '2023-10-02');
+
+INSERT into TMatricula(idAluno, idCurso, dataMatricula)
+VALUES(1, 2, '2023-10-03');
+
+
+
+--select * from TMatricula;
+
+-- mostrar todos os alunos matriculados e em quais cursos
+
+SELECT A.nome, C.nome, M.dataMatricula
+FROM TAluno A
+INNER JOIN TMatricula M
+ON A.id = M.idAluno
+INNER JOIN TCurso C
+ON M.idCurso = C.id;
+
+
+
